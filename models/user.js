9df3a2eeae3d67fb2fdddef4ab7afa77e0e58b97
Galina Guseva <galaguss@yandex.ru>
2022-10-19 01:05:35 +0500

    fix: models

--- models/user.js
+++ models/user.js
@@ -4,14 +4,12 @@ const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      default: 'Весёлый Роджер',
       required: true,
       minlength: 2,
       maxlength: 30,
     },
     about: {
       type: String,
-      default: 'Storm of the seas',
       minlength: 2,
       maxlength: 30,
       required: true,
@@ -19,19 +17,10 @@ const userSchema = new mongoose.Schema(
     avatar: {
       type: String,
       required: true,
-      default:
-        'https://st.depositphotos.com/1033409/2954/i/600/depositphotos_29548213-stock-photo-jolly-roger.jpg',
     },
   },
   {
-    toObject: {
-      versionKey: false,
-    },
-  },
-  {
-    toObject: {
-      versionKey: false,
-    },
+    versionKey: false,
   },
 );
 
