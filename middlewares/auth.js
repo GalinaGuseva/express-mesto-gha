2c3c356f9d820948f85beff87e520d518cc2bbff
Galina Guseva <galaguss@yandex.ru>
2022-10-29 02:37:10 +0500

    fix: auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,19 +2,13 @@ const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('./errors/unauthorized-err');
 
 const auth = (req, res, next) => {
-  const { authorization } = req.headers;
-
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new UnauthorizedError('Необходима авторизация'));
-  }
-
-  const token = authorization.replace('Bearer ', '');
+  const token = req.cookies.jwt;
   let payload;
 
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    return next(new UnauthorizedError('Необходима авторизация'));
+    next(new UnauthorizedError('Необходима авторизация'));
   }
 
   req.user = payload;
