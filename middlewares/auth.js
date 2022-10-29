7e1f8575d52724ecba97ab30ea81e743d0740b81
Galina Guseva <galaguss@yandex.ru>
2022-10-30 00:59:46 +0500

    auth again again

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,24 +1,26 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('./errors/unauthorized-err');
 
-module.exports = (req, res, next) => {
+const auth = (req, res, next) => {
   const token = () => {
     if (req.cookies.jwt) {
       return req.cookies.jwt;
     } if (req.headers.authorization) {
       return req.headers.authorization.replace('Bearer ', '');
-    }
-    next(new UnauthorizedError('Необходима авторизация'));
+    } return next(new UnauthorizedError('Необходима авторизация'));
   };
+
   let payload;
 
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    next(new UnauthorizedError('Необходима авторизация'));
+    return next(new UnauthorizedError('Необходима авторизация'));
   }
 
   req.user = payload;
 
   return next();
 };
+
+module.exports = auth;
